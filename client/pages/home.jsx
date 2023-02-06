a310b7da66e93cae2dd027a2223b4b491c621425
James Yang <jamesdaeyang@gmail.com>
2023-02-06 03:29:34 +0000

    Fixed issue of incorrect props values passed to EachCard component's modal and removeLocation function. #6

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -62,7 +62,7 @@ export default function Home() {
               <nav className='stickytab backwhite'>
                 <div className='nav nav-tabs nav-fill' id='nav-tab' role='tablist'>
                   <button className='nav-link active' id='nav-places-tab' data-bs-toggle='tab' data-bs-target='#nav-places' type='button' role='tab' aria-controls='nav-places' aria-selected='true' onClick={() => setViewingIds(null)}>Places</button>
-                  <button className='nav-link' id='nav-mylist-tab' data-bs-toggle='tab' data-bs-target='#nav-mylist' type='button' role='tab' aria-controls='nav-mylist' aria-selected='false' onClick={() => setViewingIds(addedLocations.map(location => location.locationId))}>My List</button>
+                  <button className='nav-link' id='nav-mylist-tab' data-bs-toggle='tab' data-bs-target='#nav-mylist' type='button' role='tab' aria-controls='nav-mylist' aria-selected='false' onClick={() => setViewingIds(null)}>My List</button>
                   <button className='nav-link' id='nav-routes-tab' data-bs-toggle='tab' data-bs-target='#nav-routes' type='button' role='tab' aria-controls='nav-routes' aria-selected='false'>My Routes</button>
                 </div>
               </nav>
