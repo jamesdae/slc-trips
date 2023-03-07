9766777f15b2ce2dcc820538ba26c0df869a2f7c
James Yang <jamesdaeyang@gmail.com>
2023-03-07 06:40:44 +0000

    Conditionally rendered Home component based on state set in App by Login component. #25

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './pages/home';
+import Login from './pages/login-form';
 
 export default function App() {
-  return <Home />;
+  const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      {user ? <Home user={user}/> : <Login onLogin={userInfo => setUser(userInfo)} />}
+    </div>
+  );
 }
