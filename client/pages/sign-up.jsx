3939e7cd0069d296011f89ab9340dd7844747319
James Yang <jamesdaeyang@gmail.com>
2023-03-09 07:42:28 +0000

    Installed new dependency for validation purposes. #25

--- client/pages/sign-up.jsx
+++ client/pages/sign-up.jsx
@@ -4,13 +4,20 @@ export default function SignUp({ showLogIn }) {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  // const [isValid, setIsValid] = useState(null);
-  // const [isTouched, setIsTouched] = useState(null);
+  const [emailIsValid, setEmailIsValid] = useState(true);
 
   const API_URL = '/api/auth';
 
+  const validateEmail = address => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // eslint-disable-next-line no-console
+    console.log(re.test(address));
+    setEmailIsValid(re.test(address));
+  };
+
   const handleSubmit = event => {
     event.preventDefault();
+    validateEmail(email);
     const request = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -21,6 +28,11 @@ export default function SignUp({ showLogIn }) {
       .then(newUser => {
         // eslint-disable-next-line no-console
         console.log('newUser', newUser);
+        if (newUser.error) {
+          setEmailIsValid(false);
+          setEmail('');
+          return;
+        }
         setEmail('');
         setUsername('');
         setPassword('');
@@ -39,11 +51,17 @@ export default function SignUp({ showLogIn }) {
           <form autoComplete="off" onSubmit={handleSubmit}>
             <div className="mb-3">
               <label htmlFor="email" className="col-form-label">Email:</label>
-              <input type="text" id="email" value={email} onChange={event => setEmail(event.target.value)} />
+              <input type="text" id="email" value={email} onChange={event => {
+                setEmail(event.target.value);
+                setEmailIsValid(true);
+              }} className={`form-control ${!emailIsValid ? 'is-invalid' : ''}`} />
+              {(!emailIsValid && email.length === 0) && <div className="invalid-feedback">Please enter a valid email address.</div>}
             </div>
             <div className="mb-3">
               <label htmlFor="username" className="col-form-label">Username:</label>
-              <input type="text" id="username" value={username} onChange={event => setUsername(event.target.value)} />
+              <input type="text" id="username" value={username} onChange={event => {
+                setUsername(event.target.value);
+              }} />
             </div>
             <div className="mb-3">
               <label htmlFor="password" className="col-form-label">Password:</label>
